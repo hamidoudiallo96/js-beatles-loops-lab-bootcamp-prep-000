613362a63c7ef09cbe8d673c0301e8d5d9a10a9d
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 18:08:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var theBeatlesPlay = (arr1,arr2) =>{
   let j;
   for(let i = 0; i<arr1.length; i++){
     j = i
-    arr.push(`${arr1[i]} plays ${arr2[j]}`)
+    newArr.push(`${arr1[i]} plays ${arr2[j]}`)
   }
   return newArr;
 }
