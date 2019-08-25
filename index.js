6830aa553b733661b0a6ffe3b73ab059bafd0693
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-25 18:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ var theBeatlesPlay = (arr1,arr2){
   return newArr;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
-var johnLennonFacts = 
+var johnLennonFacts = arr =>{
+  let newArr =[]
+  for(let i = 0; i<facts.length; i++){
+    newArr.push(`${facts[i]}!!!`)
+  }
+  return newArr
+}
